49916ef805aaa0aa0d88eef3fba6b85566d66d54
Hannah Gray <hannah.rose.gray@gmail.com>
2020-04-19 19:09:51 -0500

    Added header, footer and calculator components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FooterComponent } from './footer/footer.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CalculatorComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
