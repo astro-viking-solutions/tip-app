bf17218f8da33935dae265c588d2237983323ebb
Hannah Gray <hannah.rose.gray@gmail.com>
2020-04-21 12:52:17 -0500

    Took out "onAmountChange()" function and had the tip calculations perform on the two way variable "amount" dynamically

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,33 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
   amount: number;
-  tenPercentTip: number;
-  tenPercentTotal: number;
-  fifteenPercentTip: number;
-  fifteenPercentTotal: number;
-  twentyPercentTip: number;
-  twentyPercentTotal: number;
-  twentyfivePercentTip: number;
-  twentyfivePercentTotal: number;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onAmountChange() {
-    this.tenPercentTip = this.amount * .1;
-    this.tenPercentTotal = this.amount + (this.amount * .1);
-    this.fifteenPercentTip = this.amount * .15;
-    this.fifteenPercentTotal = this.amount + (this.amount * .15);
-    this.twentyPercentTip = this.amount * .2;
-    this.twentyPercentTotal = this.amount + (this.amount * .2);
-    this.twentyfivePercentTip = this.amount * .25;
-    this.twentyfivePercentTotal = this.amount + (this.amount * .25);
-  }
-
-  convertString() {
-
   }
 
 }
